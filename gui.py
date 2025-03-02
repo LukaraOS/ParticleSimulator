b773fe58b76ae18a0cd5ee7a79460b78185b6061
LukaraOS <lucas.th2902@gmail.com>
2025-03-02 21:04:31 +0100

    Correction de méthode Bounce(), ajout liste Particules, docString des méthodes de Particule.

--- gui.py
+++ gui.py
@@ -9,11 +9,12 @@ pygame.display.set_caption('Simulation')
 screen.fill(background_colour)
 black = (0,0,0)
 
+#MAIN BOX DEFINITION
 box = pygame.Rect(400,200,400,400)
-boxBorderLeft = 400
-boxBorderTop = 200
-boxBorderBot = 600
-boxBorderRight = 800
+boxBorderLeft = box.left
+boxBorderTop = box.top
+boxBorderBot = box.bottom
+boxBorderRight = box.right
 
 class Particle:
     def __init__(self, x, y, size):
@@ -23,7 +24,7 @@ class Particle:
         self.speed = 1
         self.angle = 0
 
-        self.angle = math.pi / 11
+        self.angle = -math.pi/2 
         self.colour = (0, 0, 255)
         self.thickness = 1
 
@@ -31,8 +32,11 @@ class Particle:
         pygame.draw.circle(screen, self.colour, (self.x, self.y), self.size, self.thickness)
 
     def move(self):
-      self.x += math.sin(self.angle) * self.speed
-      self.y -= math.cos(self.angle) * self.speed
+      """Increments x and y position coordinates"""
+
+      self.x += math.cos(self.angle) * self.speed
+      self.y += math.sin(self.angle) * self.speed
+
 
     def bounce(self):
       """Checks for collision with box borders
@@ -40,27 +44,36 @@ class Particle:
 
       if self.x > boxBorderRight - self.size:
          self.x = 2 * (boxBorderRight - self.size) - self.x
-         self.angle = - self.angle
-      elif self.x < boxBorderLeft+self.size:
-         self.x = 2 * (boxBorderLeft+self.size) - self.x
-         self.angle = - self.angle
-      if self.y > boxBorderBot - self.size:
-         self.y = 2 * (boxBorderBot - self.size) - self.y
-         self.angle = math.pi - self.angle
-      elif self.y < boxBorderTop+self.size:
-         self.y = 2 * (boxBorderTop + self.size) - self.y
          self.angle = math.pi - self.angle
 
+      elif self.x < boxBorderLeft+self.size:
+         self.x = 2 * (boxBorderLeft+self.size) - self.x
+         self.angle = math.pi - self.angle
+
+      if self.y > boxBorderBot - self.size:
+         self.y = 2 * (boxBorderBot - self.size) - self.y
+         self.angle = -self.angle
+
+      elif self.y < boxBorderTop+self.size:
+         self.y = 2 * (boxBorderTop + self.size) - self.y
+         self.angle = -self.angle
+
     def moveTowardMouse(self):
+       """Main movement method
+       Checks for mouse presence in box
+       Calls movement based on mouse position"""
        position = pygame.mouse.get_pos()
-       if position[0]<boxBorderLeft or position[0]>boxBorderRight or position[1]>boxBorderBot or position[1]<boxBorderTop:
+       
+       if position[0]<boxBorderLeft or position[0]>boxBorderRight or position[1]>boxBorderBot or position[1]<boxBorderTop:         
           self.move()
+
        else:
-         self.bounce()
-         self.angle = self.findAngle(position[0],position[1])
+         if position[1] >= self.y:
+            self.angle = self.findAngle(position[0],position[1])
+         else:
+            self.angle = self.findAngle(position[0],position[1])*-1
          self.move()
-         #self.x += math.sin(self.angle) * self.speed
-         #self.y -= math.cos(self.angle) * self.speed
+
        
     def update(self):
        self.display()
@@ -68,13 +81,12 @@ class Particle:
        self.moveTowardMouse()
 
     def findAngle(self,x,y):
-      """director_Vector = (1,0)
-      dotProduct = x
-      dist = math.sqrt((self.x+x)**2+(self.y+y)**2)
-      angle = math.acos(dotProduct/dist)"""
+      """Finds angle between particle center
+      and a point (x,y)
+      Returns an angle between 0 and Pi"""
 
-      direct = (x-self.x,y-self.y) 
-      angle = math.atan2(direct[1],direct[0])
+      direct = (x-self.x,math.sqrt((y-self.y)**2+(x-self.x)**2)) 
+      angle = math.acos(direct[0]/direct[1])
       return angle
       
 
@@ -84,19 +96,18 @@ class Particle:
        delta_y = y - self.y
 
        return (1/50 * delta_x, 1/50 * delta_y)
-       
-def refresh_screen(particle):
-  
-   particle.update()
-
-first_particle = Particle(100,100,10)
+   
+first_particle = Particle(100,100,20)
 second_particle = Particle(200,150,12)
 
+Particle_List = []
+Particle_List.append(first_particle)
+Particle_List.append(second_particle)
 running = True
 
 while running:
-  refresh_screen(first_particle)
-  refresh_screen(second_particle)
+  for particle in Particle_List:
+     particle.update() 
   pygame.display.flip()
   screen.fill(background_colour)
   pygame.draw.rect(screen,black,box,1)
