72e64a7dd9edca355f1b6f1fdfcedc6019450c00
LukaraOS <lucas.th2902@gmail.com>
2025-03-03 21:57:20 +0100

    Création de la classe Screen, module de GUI avec système d'update.

--- gui.py
+++ gui.py
@@ -1,27 +1,40 @@
 import pygame
 import math
 
-background_colour = (255,255,255)
-(width, height) = (1200, 800)
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Simulation')
-screen.fill(background_colour)
-black = (0,0,0)
 
-#MAIN BOX DEFINITION
-box = pygame.Rect(400,200,400,400)
-boxBorderLeft = box.left
-boxBorderTop = box.top
-boxBorderBot = box.bottom
-boxBorderRight = box.right
+class Screen:
+  def __init__(self,width,height,boxSize):
+    self.width = width
+    self.height = height
 
+    #MAIN BOX DEFINITION
+    self.boxSize = boxSize
+    self.box = pygame.Rect((self.width-self.boxSize)/2,(self.height-self.boxSize)/2,self.boxSize,self.boxSize)
+    self.background_colour = (255,255,255)
+    
+    self.boxBorderLeft = self.box.left
+    self.boxBorderTop = self.box.top
+    self.boxBorderBot = self.box.bottom
+    self.boxBorderRight = self.box.right
+
+  def drawBox(self): #Draw our box 
+    pygame.draw.rect(self.screen,(0,0,0),self.box,1)
+
+  def show(self): #Call this first to init the screen 
+    self.screen = pygame.display.set_mode((self.width,self.height))
+    self.update()
+
+  def update(self):
+    pygame.display.flip()
+    self.screen.fill(self.background_colour)
+    self.drawBox()
+
+
+screen = Screen(1200,800,400)
+screen.show()
 running = True
 while running:
-
-  pygame.display.flip()
-  screen.fill(background_colour)
-  pygame.draw.rect(screen,black,box,1)
-
+  screen.update()
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
