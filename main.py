5771159db018bd3bca8c222a8e9afff9d2244b10
LukaraOS <lucas.th2902@gmail.com>
2025-03-03 21:56:20 +0100

    Correction des dunder method de Vector2

--- main.py
+++ main.py
@@ -6,34 +6,36 @@ class Vector2:
         self.y = y
 
     def __add__(self, vec1):
-        vec2 = (self.x + vec1.x, self.y + vec1.y)
+        vec2 = Vector2(self.x + vec1.x, self.y + vec1.y)
         return vec2
     
     def __neg__(self): 
-        vec1 = (-self.x,-self.y)
+        vec1 = Vector2(-self.x,-self.y)
         return vec1
+    
+    def __str__(self): #Defines the output upon calling str method on a Vector2 object i.e : print( Vector2 object ) --> (x,y)
+        coordinates = f"({self.x},{self.y})"
+        return coordinates
     
     def __mul__(self, vec1): # /!\ Dot Product not Vectorial Product /!\
         dotProduct = self.x * vec1.x + self.y * vec1.y     
         return dotProduct
     
     def __sub__(self, vec1):
-        vec2 = (self.x - vec1.x, self.y - vec1.y)
+        vec2 = Vector2(self.x - vec1.x, self.y - vec1.y)
         return vec2
 
 class Particle:
     def __init__(self, x, y):
         self.position = Vector2(x,y)
-        self.acceleration = Vector2(1,1)
+        self.acceleration = Vector2(-1,-1)
         self.velocity = Vector2(1,1)
         self.gravity = Vector2(0,-9.81)
     
     def movement(self):
         self.velocity = self.velocity + self.acceleration
-        self.position = (self.position + self.velocity) + self.gravity
+        self.position = self.position + self.velocity + self.gravity
         
-
-
 
 
 particle = Particle(0,0)
